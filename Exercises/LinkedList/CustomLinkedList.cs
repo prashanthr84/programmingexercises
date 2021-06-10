c0bf3563f6435d8eecf2975a7dd2fddff8d05c1e
Rao <Prashanth.Rao@philips.com>
2021-06-10 18:36:44 +0530

    exercise for implementing custom linked list

--- Exercises/LinkedList/CustomLinkedList.cs
+++ Exercises/LinkedList/CustomLinkedList.cs
@@ -8,45 +8,25 @@ namespace Exercises.LinkedList {
     /// </summary>
     public class CustomLinkedList {
 
-        public CustomNode First { get; private set; }
-        public CustomNode Last { get; private set; }
+        public CustomNode First { get; set; }
+
+        public CustomNode Last { get; set; }
 
         public void AddFirst(CustomNode node) {
-            if (node is null) {
-                throw new ArgumentNullException(nameof(node));
-            }
+            
 
             if (string.IsNullOrWhiteSpace(node.Data)) {
                 throw new InvalidNodeDataException();
             }
 
-            if (First is null) {
-                First = node;
-                Last = node;
-            } else {
-                node.Previous = null;
-                node.Next = First;
-                First = node;
-            }
         }
 
         public void AddLast(CustomNode node) {
-            if (node is null) {
-                throw new ArgumentNullException(nameof(node));
-            }
 
             if (node.Data is null) {
                 throw new InvalidNodeDataException();
             }
 
-            if (Last == null) {
-                First = node;
-                Last = node;
-            } else {
-                node.Previous = Last;
-                node.Next = null;
-                Last = node;
-            }
         }
     }
 }
